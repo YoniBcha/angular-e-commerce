b64d0e2aedc7c3fc3a56223888ebd11791fa42e6
YoniBcha <yonasmillion29@gmail.com>
2026-03-28 23:51:34 +0900

    auth file is added with bg

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,11 +4,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
-  template: `
-    <h1>Home Page</h1>
-    <a routerLink="/login">Login</a>
-    <a routerLink="/profile">Profile</a>
-  `,
+  imports: [],
+  templateUrl: './home.html',
 })
 export class HomeComponent {}
