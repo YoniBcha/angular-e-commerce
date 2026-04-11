65f5344c8622b924bc2f449d2e97680f611cdf07
YoniBcha <yonasmillion29@gmail.com>
2026-04-11 08:56:09 -0700

    top bar and also nav-bar also added

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ImageSliderComponent } from '../../shared/image-slider/image-slider';
+import { NavBar } from '../nav-bar/nav-bar';
+import { TopBar } from '../top-bar/top-bar';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ImageSliderComponent],
+  imports: [ImageSliderComponent, NavBar, TopBar],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
